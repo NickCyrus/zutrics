d70f44f09ce869392788a74325cb7dd0dc011c09
NickCyrus <nicklemusduque@gmail.com>
2021-09-16 15:05:02 -0500

    Actualización

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost:8000'),
+    'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL', 'http://localhost:8000'),
+    'asset_url' => env('ASSET_URL', null),
 
     /*
     |--------------------------------------------------------------------------
